343e668e2d3d0c638d75fa30a17d96c843859a40
Frankdias92 <frankmcdias@gmail.com>
2024-12-11 22:08:35 -0300

    :bulb: base structure

--- main.go
+++ main.go
@@ -5,14 +5,5 @@ import (
 )
 
 func main() {
-	x := doDefer()
-	fmt.Println(x)
-}
-
-func doDefer() int {
-	defer fmt.Println(3)
-	defer fmt.Println(2)
-	fmt.Println(1)
-
-	return 10
+	fmt.Println("hello")
 }
