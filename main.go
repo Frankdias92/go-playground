e4e09cbe9f62722976c4e7de2b54c6dc01c16864
Frankdias92 <frankmcdias@gmail.com>
2024-12-11 21:44:29 -0300

    :bulb: if & switch cases

--- main.go
+++ main.go
@@ -2,8 +2,56 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+// "math"
+
 func main() {
-	fmt.Println("Hello, word!")
+	// if x := math.Sqrt(4); x < 1 {
+	// 	fmt.Println(x)
+	// } else if x > 1 {
+	// 	fmt.Println(" else value ")
+	// }
+
+	do(1)
+	// do(2)
+	// do(3)
+
+	fmt.Println(isWeekend(time.Now()))
+
 }
+
+func do(x int) {
+	switch x {
+	case 1:
+		fmt.Println(1)
+		fallthrough
+	case 2:
+		fmt.Println(2)
+	default:
+		fmt.Println("Another thing")
+	}
+}
+
+func isWeekend(x time.Time) bool {
+	switch {
+	case x.Weekday() > 0 && x.Weekday() < 6:
+		return false
+	default:
+		return true
+	}
+}
+
+// func doType(x any) {
+// 	switch t := x.(type) {
+// 	case string:
+// 		takeString(t)
+// 	case int:
+// 	case nil:
+// 	}
+// }
+
+// func takeString(s string) {
+// 	fmt.Println(s)
+// }
