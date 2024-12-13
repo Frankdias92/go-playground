6795709de1babe0ec8e908bf77234f0bc87b4a66
Frankdias92 <frankmcdias@gmail.com>
2024-12-13 10:44:23 -0300

    :bulb: Docs: adding arrays and slices

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"myFirstProject/arrays"
 )
 
 func main() {
 	fmt.Println("hello")
+	arrays.GetArrayAndSlice()
+
+	fmt.Println("Print slice")
+	// sliceWitoutIndexThree := []int{0,1,2} // this will cause a panic because there is no index on element 3
+	sliceWithIndex := []int{0, 1, 2, 3}
+	arrays.IsInbounds(sliceWithIndex)
 }
