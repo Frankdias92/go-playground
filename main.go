6697e233cf37ac4e449be312e2142fa38e94e4ca
Frankdias92 <frankmcdias@gmail.com>
2024-12-21 10:34:04 -0300

    :bulb: Doc: Reoganize errorhandler structure

--- main.go
+++ main.go
@@ -1,56 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"time"
-
-	condition "github.com/frankdias92/myFirstProject/Condition"
-	function "github.com/frankdias92/myFirstProject/Function"
-	loops "github.com/frankdias92/myFirstProject/Loops"
-	quizproject "github.com/frankdias92/myFirstProject/QuizProject"
-	structs "github.com/frankdias92/myFirstProject/Structs"
-	"github.com/frankdias92/myFirstProject/arrays"
 	errorhandler "github.com/frankdias92/myFirstProject/errorHandler"
-	maplesson "github.com/frankdias92/myFirstProject/mapLesson"
 )
 
-func Test() {
-	fmt.Println("Hello Dev, be welcome on my mess")
-	arrays.GetArrayAndSlice()
-
-	/* Arrays */
-	sliceWithIndex := []int{0, 1, 2, 3}
-	arrays.IsInbounds(sliceWithIndex)
-
-	/* My first Game in Go */
-	/* guesgame.PlayGameGuess() */
-
-	function.MakeFunction()
-	fmt.Println(function.Divid(2, 2))
-	fmt.Println(function.Sum(2, 2))
-	fmt.Println(function.Swap(1, 2))
-
-	/* if condition */
-	condition.MySuperCondition()
-	fmt.Println(condition.IsWeekend(time.Time{}))
-	condition.GetResult(6)
-
-	/* loops */
-	loops.SuperLoop()
-	loops.RangeBased()
-	loops.GoRoutinesLoop()
-	loops.Matrix()
-
-	/* map */
-	maplesson.GetMap()
-	maplesson.ForMap()
-
-	/*  struct  */
-	structs.Struct()
-	structs.Interface()
-	structs.SwitchStatement()
-	structs.TypeStruct()
-
+func main() {
 	/* Error handling */
 	errorhandler.HandleError()
 	errorhandler.Error1()
@@ -59,9 +13,4 @@ func Test() {
 	errorhandler.Error4()
 	errorhandler.Error5()
 	errorhandler.Reader()
-}
-
-func main() {
-	// Run the quiz game
-	quizproject.AskQuestion()
 }
