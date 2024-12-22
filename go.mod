20d8a53fd0d0f33d28f1692e36b71559abebe0f7
Frankdias92 <frankmcdias@gmail.com>
2024-12-22 16:24:58 -0300

    :sparkles: Feat: implementing server

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module go-playgraound
+module github.com/frankdias92/go-playground
 
 go 1.23.4
 
--- main.go
+++ main.go
@@ -1,8 +1,35 @@
 package main
 
-import apirest "go-playgraound/api-rest"
+import (
+	"log/slog"
+	"net/http"
+	"time"
+
+	api "github.com/frankdias92/go-playground/api-project"
+)
 
 func main() {
-	// apirest.ApiRest()
-	apirest.GetJson()
+	if err := run(); err != nil {
+		slog.Error("failed to execute code", "error", err)
+		return
+	}
+	slog.Info("all system offline")
+}
+
+func run() error {
+	handler := api.NewHandler()
+
+	server := http.Server{
+		ReadTimeout:  10 * time.Second,
+		IdleTimeout:  time.Minute,
+		WriteTimeout: 10 * time.Second,
+		Addr:         ":8085",
+		Handler:      handler,
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		return err
+	}
+
+	return nil
 }
