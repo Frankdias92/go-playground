5ed473cbc74a82278b4032e3b29814dc62302155
Frankdias92 <frankmcdias@gmail.com>
2025-01-11 17:54:44 -0300

    :sparkles: Feat: creating user routers

---
+++ internal/api/userHandlers.go
@@ -0,0 +1,15 @@
+package api
+
+import "net/http"
+
+func (api *Api) handleSignUpUser(w http.ResponseWriter, r *http.Request) {
+	panic("TODO")
+}
+
+func (api *Api) handlesLoginUser(w http.ResponseWriter, r *http.Request) {
+	panic("todo")
+}
+
+func (api *Api) handleLogOut(w http.ResponseWriter, r *http.Request) {
+	panic("todo")
+}
